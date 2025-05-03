8933cbc07ef0d32f00e37765d4a72f6df9308d92
Tomas Vainstein <tvainstein@fi.uba.ar>
2025-05-03 00:51:02 -0300

    visualizacion en seccion mis cursos y tag de inscripciones en la seccion de cursos

--- api/coursesClient.ts
+++ api/coursesClient.ts
@@ -5,8 +5,8 @@ import {Course} from "@/types/Course";
 // Configura la URL base de la API
 // En desarrollo con Expo, puedes usar la IP de tu máquina en lugar de localhost
 const getBaseUrl = (): string => {
-    const LOCAL_IP = "192.168.100.25";
-    //const LOCAL_IP = "35.223.247.76";
+    //const LOCAL_IP = "192.168.100.25";
+    const LOCAL_IP = "35.223.247.76";
     return `http://${LOCAL_IP}:3000`;
 }
 
@@ -80,10 +80,10 @@ export const courseClient = {
     isEnrolledInCourse: async (courseId: string, userId: string) => {
         try {
             const response = await api.get(`/courses/${courseId}/enrollments/${userId}`)
-            return response.data
+            return response.data.isEnrolled === true
         } catch (error) {
             console.error(`Error checking enrollment for user ${userId} in course ${courseId}:`, error)
-            throw error
+            return false
         }
     },
 
